42de8b12b812ad3333f6ab1d372481bcfb9aa3f4
Nicolás Hernández <nm.hernandez10@uniandes.edu.co>
2018-03-21 13:31:21 -0500

    EspacioResource

--- src/rest/EspacioService.java
+++ src/rest/EspacioService.java
@@ -1,11 +1,77 @@
 package rest;
 
-import java.sql.Date;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import tm.AlohAndesTransactionManager;
+import vos.Espacio;
+import vos.ListaEspacios;
 
 
-
+@Path("espacios")
 public class EspacioService 
 {
-		
+	@Context
+	private ServletContext context;
 	
+	private String getPath() 
+	{
+		return context.getRealPath("WEB-INF/ConnectionData");
+	}
+	
+	private String doErrorMessage(Exception e)
+	{
+		return "{ \"ERROR\": \""+ e.getMessage() + "\"}" ;
+	}
+	
+	//RFC2
+	
+	@GET
+	@Path("/espaciospopulares")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response getBoletas() 
+	{
+		AlohAndesTransactionManager tm = new AlohAndesTransactionManager(getPath());
+		
+		try 
+		{
+			ListaEspacios espacios = new ListaEspacios(tm.espaciosPopulares());
+			return Response.status(200).entity(espacios).build();
+		} 
+		catch (Exception e) 
+		{
+			return Response.status(500).entity(doErrorMessage(e)).build();
+		}		
+	}
+	
+	//RF6
+	
+	@DELETE
+	@Path("/espacio")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteEspacio(Espacio espacio) 
+	{
+		AlohAndesTransactionManager tm = new AlohAndesTransactionManager(getPath());
+		
+		try 
+		{
+			Date fechaCancelacion = new Date();
+			tm.cancelarEspacio(espacio, fechaCancelacion);
+			return Response.status(200).entity(espacio).build();
+		} 
+		catch (Exception e) 
+		{
+			return Response.status(500).entity(doErrorMessage(e)).build();
+		}		
+	}	
 }
