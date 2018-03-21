8314c809d94f8193aaf5b16e5558ba14cd171bac
Nicolás Hernández <nm.hernandez10@uniandes.edu.co>
2018-03-20 22:23:24 -0500

    vos package terminado

--- src/vos/Espacio.java
+++ src/vos/Espacio.java
@@ -1,10 +1,8 @@
 package vos;
 
 import java.sql.Date;
+import org.codehaus.jackson.annotate.*;
 
-import rest.JsonProperty;
-
-import vos.JsonProperty;
 
 /**
  * @author Nicolás Mateo Hernández Rojas 	- 	nm.hernandez10@uniandes.edu.co
@@ -26,7 +24,7 @@ public class Espacio
 	@JsonProperty(value="tamaño")
 	private double tamaño;
 	
-	@JsonProperty(value="ubicación")
+	@JsonProperty(value="ubicacion")
 	private String ubicacion;
 	
 	@JsonProperty(value="precio")
@@ -35,5 +33,84 @@ public class Espacio
 	@JsonProperty(value="fechaRetiro")
 	private Date fechaRetiro;
 	
-	
+	public Espacio(@JsonProperty(value="id")long id,@JsonProperty(value="registro")long registro, @JsonProperty(value="capacidad")int capacidad, @JsonProperty(value="tamaño")double tamaño, @JsonProperty(value="ubicacion")String ubicacion, @JsonProperty(value="precio")double precio,@JsonProperty(value="fechaRetiro")Date fechaRetiro )
+	{
+		this.id = id;
+		this.registro = registro;
+		this.capacidad = capacidad;
+		this.tamaño = tamaño;
+		this.ubicacion = ubicacion;
+		this.precio = precio;
+		this.fechaRetiro = fechaRetiro;
+	}
+
+	public long getId()
+	{
+		return id;
+	}
+
+	public void setId(long id) 
+	{
+		this.id = id;
+	}
+
+	public long getRegistro()
+	{
+		return registro;
+	}
+
+	public void setRegistro(long registro)
+	{
+		this.registro = registro;
+	}
+
+	public int getCapacidad() 
+	{
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) 
+	{
+		this.capacidad = capacidad;
+	}
+
+	public double getTamaño() 
+	{
+		return tamaño;
+	}
+
+	public void setTamaño(double tamaño) 
+	{
+		this.tamaño = tamaño;
+	}
+
+	public String getUbicacion() 
+	{
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) 
+	{
+		this.ubicacion = ubicacion;
+	}
+
+	public double getPrecio() 
+	{
+		return precio;
+	}
+
+	public void setPrecio(double precio)
+	{
+		this.precio = precio;
+	}
+
+	public Date getFechaRetiro()
+	{
+		return fechaRetiro;
+	}
+
+	public void setFechaRetiro(Date fechaRetiro) 
+	{
+		this.fechaRetiro = fechaRetiro;
+	}	
 }
--- src/vos/Habitacion.java
+++ src/vos/Habitacion.java
@@ -1,6 +1,79 @@
 package vos;
 
+import org.codehaus.jackson.annotate.*;
+
+/**
+ * @author Nicolás Mateo Hernández Rojas 	- 	nm.hernandez10@uniandes.edu.co
+ * @author David Felipe Niño Romero		-	df.nino10@uniandes.edu.co
+ * Clase que representa a las Habitaciones del modelo AlohAndes
+ */
+
 public class Habitacion 
 {
+	@JsonProperty(value = "id")
+	private long id;
+	
+	public enum CategoriaHabitacion
+	{
+		ESTÁNDAR,
+		SEMISUITE,
+		SUITE
+	}
+	
+	@JsonProperty(value = "categoria")
+	private CategoriaHabitacion categoria;
+	
+	@JsonProperty(value = "compartido")
+	private boolean compartido;
+	
+	@JsonProperty(value = "capacidad")
+	private int capacidad;
+	
+	public Habitacion(@JsonProperty(value = "id")long id, @JsonProperty(value = "categoria")CategoriaHabitacion categoria, @JsonProperty(value = "compartido")boolean compartido, @JsonProperty(value = "capacidad")int capacidad)
+	{
+		this.id = id;
+		this.capacidad = capacidad;
+		this.categoria = categoria;
+		this.compartido = compartido;
+	}
 
+	public long getId() 
+	{
+		return id;
+	}
+
+	public void setId(long id) 
+	{
+		this.id = id;
+	}
+
+	public CategoriaHabitacion getCategoria()
+	{
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaHabitacion categoria)
+	{
+		this.categoria = categoria;
+	}
+
+	public boolean isCompartido()
+	{
+		return compartido;
+	}
+
+	public void setCompartido(boolean compartido) 
+	{
+		this.compartido = compartido;
+	}
+
+	public int getCapacidad()
+	{
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) 
+	{
+		this.capacidad = capacidad;
+	}	
 }
